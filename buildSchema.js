0403956f2c930960d8ea104f132de05688868db9
Julian Schoepf <ferlojulian@hotmail.com>
2022-07-07 11:15:23 +0200

    React GraphQL-Express setup

---
+++ buildSchema.js
@@ -0,0 +1,1 @@
+const GraphQlObjectType = require('graphql');
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React-Relay-GraphQL Quiz</h1>
+    </div>
+  );
+}
+
+export default App;
